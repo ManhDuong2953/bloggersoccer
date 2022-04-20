75bc817c45f7d7f8804dca6bf694b5a0468a2ed1
Duong Manh <manhme2953@gmail.com>
2022-04-20 07:26:00 +0700

    empty footer

--- js/hoverDate.js
+++ js/hoverDate.js
@@ -1,31 +1,55 @@
 const imgSmalls =  document.querySelectorAll(".img-new-1")
 const imgBig =  document.querySelector(".img-new-2")
-const btnDate =  document.querySelectorAll(".btn-small")
-const btnDateBig =  document.querySelector(".btn-middle")
-const textNews = document.querySelectorAll(".text-new")
-var address =0;
-imgSmalls.forEach((imgSmall, index)=>{
-    imgSmall.addEventListener("mouseover" ,()=>{
-        btnDate[index].classList.add("hover-btn-new")
-        if(index>=4){
-            textNews[index-4].classList.add('text-new--hover')
-        } 
-        imgSmall.addEventListener("mouseleave" ,()=>{
-            btnDate[index].classList.remove("hover-btn-new")
-            if(index>=4){
-                 textNews[index-4].classList.remove('text-new--hover') 
-            } 
+var btnDate =  document.querySelectorAll(".btn-small")
+var btnDateBig =  document.querySelector(".btn-middle")
+const textNewsjs = document.querySelectorAll(".text-new-js")
+var textNews = document.querySelectorAll(".title-new")
+
+
+imgSmalls.forEach((imgSmall, index) => {
+    imgSmall.addEventListener("mouseover", ()=>{
+        btnDate[index].classList.add("hover-btn-new");
+        imgSmall.addEventListener("mouseout", ()=>{
+            btnDate[index].classList.remove("hover-btn-new");
         })
     })
+});
 
-})
 
-imgBig.addEventListener("mouseover" ,()=>{
-    btnDateBig.classList.add("hover-btn-new")
-    imgBig.addEventListener("mouseleave" ,()=>{
-        btnDateBig.classList.remove("hover-btn-new")
+
+
+textNews.forEach((textNew, index) => {
+    textNew.addEventListener("mouseover", ()=>{
+        console.log(index)
+    if(index===2){
+        btnDateBig.classList.add("hover-btn-new");
+        textNew.addEventListener("mouseout", ()=>{
+            btnDateBig.classList.remove("hover-btn-new");
+        })
+    }
+    
+    else if(index!=2){
+        var num=index-1;
+        if(index<=1){
+            num=index;
+        }
+        btnDate[num].classList.add("hover-btn-new");
+        textNew.addEventListener("mouseout", ()=>{
+            btnDate[num].classList.remove("hover-btn-new");
+        })
+    }
     })
 })
 
 
+
+
+textNewsjs.forEach((textNewjs,index)=>{
+    textNewjs.addEventListener("mouseover", ()=>{
+        textNewjs.classList.add("text-new--hover");
+        textNewjs.addEventListener("mouseout", ()=>{
+            textNewsjs[index].classList.remove("text-new--hover");
+        })
+    })
+})
 
